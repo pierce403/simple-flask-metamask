90a5d0dd33f5084489c733c0fc75572452ece774
Satish Surath <s@n-m.co>
2022-07-02 08:15:53 -0400

    Adding Support for Ethereum Test Networks
    Signed-off-by: Satish Surath <s@n-m.co>

--- static/app.js
+++ static/app.js
@@ -38,34 +38,92 @@ function handleAuth(accountAddress, signature)
 }
 
 ethereum.request({ method: "eth_requestAccounts" }).then(function () {
-
     provider = new ethers.providers.Web3Provider(web3.currentProvider);
-
-
     provider.getNetwork().then(function (result) {
-        if (result['chainId'] != 1) {
-            document.getElementById("msg").textContent = 'Switch to Mainnet!';
-
-        } else { // okay, confirmed we're on mainnet
-
+        if (result['chainId'] == 4) {
+             // okay, confirmed we're on Rinkeby Network
             provider.listAccounts().then(function (result) {
                 console.log(result);
                 accountAddress = result[0]; // figure out the user's Eth address
+                provider.getBalance(String(result[0])).then(function (balance) {
+                    var myBalance = (balance / ethers.constants.WeiPerEther).toFixed(4);
+                    console.log("Your Balance: " + myBalance);
+                        document.getElementById("msg").textContent = 'RinkebyETH Balance in Rinkeby Network: ' + myBalance;
+                    });
+                    // get a signer object so we can do things that need signing
+                    signer = provider.getSigner();
+                    // build out the table of players
+                })
+        } 
+        else if (result['chainId'] == 5) {
+             // okay, confirmed we're on Goerli Network
+            provider.listAccounts().then(function (result) {
+                console.log(result);
+                accountAddress = result[0]; // figure out the user's Eth address
+                provider.getBalance(String(result[0])).then(function (balance) {
+                    var myBalance = (balance / ethers.constants.WeiPerEther).toFixed(4);
+                    console.log("Your Balance: " + myBalance);
+                        document.getElementById("msg").textContent = 'GoerliETH Balance in Goerli Network: ' + myBalance;
+                    });
+                    // get a signer object so we can do things that need signing
+                    signer = provider.getSigner();
+                    // build out the table of players
+                })
+        } 
+        else if (result['chainId'] == 42) {
+             // okay, confirmed we're on Kovan Network
+            provider.listAccounts().then(function (result) {
+                console.log(result);
+                accountAddress = result[0]; // figure out the user's Eth address
+                provider.getBalance(String(result[0])).then(function (balance) {
+                    var myBalance = (balance / ethers.constants.WeiPerEther).toFixed(4);
+                    console.log("Your Balance: " + myBalance);
+                        document.getElementById("msg").textContent = 'KovanETH Balance in Kovan Network: ' + myBalance;
+                    });
+                    // get a signer object so we can do things that need signing
+                    signer = provider.getSigner();
+                    // build out the table of players
+                })
+        }   
+        else if (result['chainId'] == 3) {
+             // okay, confirmed we're on Ropsten Network
+            provider.listAccounts().then(function (result) {
+                console.log(result);
+                accountAddress = result[0]; // figure out the user's Eth address
+                provider.getBalance(String(result[0])).then(function (balance) {
+                    var myBalance = (balance / ethers.constants.WeiPerEther).toFixed(4);
+                    console.log("Your Balance: " + myBalance);
+                        document.getElementById("msg").textContent = 'RopstenETH Balance in Ropsten Network: ' + myBalance;
+                    });
+                    // get a signer object so we can do things that need signing
+                    signer = provider.getSigner();
+                    // build out the table of players
+                })
+        }         
+        else if (result['chainId'] != 1) {
+            console.log(result);
+            document.getElementById("msg").textContent = 'Switch to Mainnet! ---- ' + result['name'];
 
+        } 
+        else
+        { // okay, confirmed we're on mainnet
+            provider.listAccounts().then(function (result) {
+                console.log(result);
+                accountAddress = result[0]; // figure out the user's Eth address
                 provider.getBalance(String(result[0])).then(function (balance) {
                     var myBalance = (balance / ethers.constants.WeiPerEther).toFixed(4);
                     console.log("Your Balance: " + myBalance);
                     document.getElementById("msg").textContent = 'ETH Balance: ' + myBalance;
                 });
-
                 // get a signer object so we can do things that need signing
                 signer = provider.getSigner();
-
                 // build out the table of players
             })
         }
     })
 })
+
+
 
 /*
 web3.eth.getAccounts()
